313d9ed7392769c5520a82d098afcff355d547d4
Joseph W. Dixon <jdixon@bignerdranch.com>
2014-12-09 16:13:42 -0600

    Adds basic FancyTable class

---
+++ Objective-C/HomeKitty/HomeKitty/FancyTable.h
@@ -0,0 +1,16 @@
+//
+//  FancyTable.h
+//  HomeKitty
+//
+//  Created by Joseph W. Dixon on 12/9/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FancyTable : UIView
+
+#pragma mark - Initializers
+- (instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style;
+
+@end
---
+++ Objective-C/HomeKitty/HomeKitty/FancyTable.m
@@ -0,0 +1,44 @@
+//
+//  FancyTable.m
+//  HomeKitty
+//
+//  Created by Joseph W. Dixon on 12/9/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "FancyTable.h"
+#import "NSLayoutConstraint+BNRQuickConstraints.h"
+
+@interface FancyTable()
+
+@property (nonatomic, weak) UIToolbar *toolbar;
+@property (nonatomic, weak) UITableView *tableView;
+
+@end
+
+@implementation FancyTable
+
+#pragma mark - Initializers
+- (instancetype)initWithFrame:(CGRect)frame style:(UITableViewStyle)style {
+    self = [super initWithFrame:frame];
+    if (self) {
+        UIToolbar *toolbar = [[UIToolbar alloc] initWithFrame:CGRectZero];
+        [self addSubview:toolbar];
+        self.toolbar = toolbar;
+        
+        UITableView *tableView = [[UITableView alloc] initWithFrame:CGRectZero style:style];
+        [self addSubview:tableView];
+        self.tableView = tableView;
+        
+        NSDictionary *views = NSDictionaryOfVariableBindings(toolbar, tableView);
+        NSString *format = @"H:|[toolbar]|,H:|[tableView]|,V:|[toolbar(44)]-0-[tableView]|";
+        [self addConstraints:[NSLayoutConstraint bnr_constraintsWithCommaDelimitedFormat:format views:views]];
+    }
+    return self;
+}
+
+- (instancetype)initWithFrame:(CGRect)frame {
+    return [self initWithFrame:frame style:UITableViewStylePlain];
+}
+
+@end
