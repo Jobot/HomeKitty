543e102da5e5a53e359cde2690882389c070f467
Joseph W. Dixon <jdixon@bignerdranch.com>
2014-12-10 11:37:29 -0600

    Now showing Add Home and Add Room dialogs

--- Objective-C/HomeKitty/HomeKitty/HomeRoomsVC.m
+++ Objective-C/HomeKitty/HomeKitty/HomeRoomsVC.m
@@ -12,7 +12,10 @@
 #import "BNRFancyTableView.h"
 #import "NSLayoutConstraint+BNRQuickConstraints.h"
 
-@interface HomeRoomsVC ()
+static NSInteger const HomeRoomsAddHomeTextFieldTag = -100;
+static NSInteger const HomeRoomsAddRoomTextFieldTag = -101;
+
+@interface HomeRoomsVC () <UITextFieldDelegate>
 
 @property (nonatomic, weak) BNRFancyTableView *homeList;
 @property (nonatomic, weak) BNRFancyTableView *roomList;
@@ -20,6 +23,8 @@
 @property (nonatomic) RoomDataSource *roomDataSource;
 @property (nonatomic) id<NSObject> homeChangeObserver;
 @property (nonatomic) id<NSObject> roomChangeObserver;
+@property (nonatomic) NSString *addedHomeText;
+@property (nonatomic) NSString *addedRoomText;
 
 @end
 
@@ -55,17 +60,29 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    // configure data sources
     self.homeDataSource = [[HomeDataSource alloc] init];
     self.roomDataSource = [[RoomDataSource alloc] init];
     
+    // configure home list
     BNRFancyTableView *homeList = self.homeList;
     homeList.dataSource = self.homeDataSource;
     homeList.translatesAutoresizingMaskIntoConstraints = NO;
+    UIBarButtonItem *addHomeButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                                                                   target:self
+                                                                                   action:@selector(didPressAddHomeButton:)];
+    [homeList addToolbarItem:addHomeButton];
     
+    // configure room list
     BNRFancyTableView *roomList = self.roomList;
     roomList.dataSource = self.roomDataSource;
     roomList.translatesAutoresizingMaskIntoConstraints = NO;
+    UIBarButtonItem *addRoomButton = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemAdd
+                                                                                   target:self
+                                                                                   action:@selector(didPressAddRoomButton:)];
+    [roomList addToolbarItem:addRoomButton];
     
+    // add constraints
     UINavigationBar *navBar = self.navigationController.navigationBar;
     NSNumber *hPad = @12;
     NSNumber *vPad = @12;
@@ -109,6 +126,72 @@
     if (self.homeChangeObserver) {
         [[NSNotificationCenter defaultCenter] removeObserver:self.homeChangeObserver];
         self.homeChangeObserver = nil;
+    }
+}
+
+#pragma mark - Actions
+
+- (void)didPressAddHomeButton:(id)sender {
+    __weak __typeof(self) weakSelf = self;
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Name your Home"
+                                                                   message:@"Each home must have a unique name. Please give your home a name."
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.delegate = self;
+        textField.placeholder = @"Home Sweet Home";
+        textField.tag = HomeRoomsAddHomeTextFieldTag;
+    }];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Add"
+                                             style:UIAlertActionStyleDefault
+                                           handler:^(UIAlertAction *action) {
+                                               NSLog(@"ADDING HOME: %@", weakSelf.addedHomeText);
+                                           }]];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                              style:UIAlertActionStyleCancel
+                                            handler:^(UIAlertAction *action) {
+                                                weakSelf.addedHomeText = nil;
+                                            }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)didPressAddRoomButton:(id)sender {
+    __weak __typeof(self) weakSelf = self;
+    
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"Name your Room"
+                                                                   message:@"Rooms within a home must be uniquely named. Please name yours"
+                                                            preferredStyle:UIAlertControllerStyleAlert];
+    
+    [alert addTextFieldWithConfigurationHandler:^(UITextField *textField) {
+        textField.delegate = self;
+        textField.placeholder = @"My Room";
+        textField.tag = HomeRoomsAddRoomTextFieldTag;
+    }];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Add"
+                                              style:UIAlertActionStyleDefault
+                                            handler:^(UIAlertAction *action) {
+                                                NSLog(@"ADDING ROOM: %@", weakSelf.addedRoomText);
+                                            }]];
+    
+    [alert addAction:[UIAlertAction actionWithTitle:@"Cancel"
+                                              style:UIAlertActionStyleCancel
+                                            handler:^(UIAlertAction *action) {
+                                                weakSelf.addedRoomText = nil;
+                                            }]];
+    
+    [self presentViewController:alert animated:YES completion:nil];
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+    if (textField.tag == HomeRoomsAddHomeTextFieldTag) {
+        self.addedHomeText = textField.text;
+    } else if (textField.tag == HomeRoomsAddRoomTextFieldTag) {
+        self.addedRoomText = textField.text;
     }
 }
 
