337f99377611c108b3c8935872415b918353db58
Joseph W. Dixon <jdixon@bignerdranch.com>
2014-12-10 10:07:38 -0600

    Adds room data source model

---
+++ Objective-C/HomeKitty/HomeKitty/RoomDataSource.h
@@ -0,0 +1,19 @@
+//
+//  RoomDataSource.h
+//  HomeKitty
+//
+//  Created by Joseph W. Dixon on 12/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class HMHome;
+
+extern NSString * const RoomDataSourceDidChangeNotification;
+
+@interface RoomDataSource : NSObject <UITableViewDataSource>
+
+@property (nonatomic) HMHome *home;
+
+@end
---
+++ Objective-C/HomeKitty/HomeKitty/RoomDataSource.m
@@ -0,0 +1,45 @@
+//
+//  RoomDataSource.m
+//  HomeKitty
+//
+//  Created by Joseph W. Dixon on 12/10/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "RoomDataSource.h"
+@import HomeKit;
+
+NSString * const RoomDataSourceDidChangeNotification = @"RoomDataSourceDidChangeNotification";
+
+@implementation RoomDataSource
+
+#pragma mark - Customer Getters / Setters
+
+- (void)setHome:(HMHome *)home {
+    _home = home;
+    [[NSNotificationCenter defaultCenter] postNotificationName:RoomDataSourceDidChangeNotification object:nil];
+}
+
+#pragma mark - Table View Data Source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return [self.home.rooms count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"RoomCell"];
+    if (!cell) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"RoomCell"];
+    }
+    
+    HMRoom *room = self.home.rooms[indexPath.row];
+    cell.textLabel.text = room.name;
+    
+    return cell;
+}
+
+@end
