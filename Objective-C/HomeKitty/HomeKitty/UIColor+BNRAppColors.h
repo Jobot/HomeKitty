3dc7d09b6954959630c604f740ebee84ae8bfd9b
Joseph W. Dixon <jdixon@bignerdranch.com>
2014-12-12 10:27:10 -0600

    Adds custom background color for app

---
+++ Objective-C/HomeKitty/HomeKitty/UIColor+BNRAppColors.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+BNRAppColors.h
+//  HomeKitty
+//
+//  Created by Joseph W. Dixon on 12/12/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (BNRAppColors)
+
++ (UIColor *)bnr_backgroundColor;
+
+@end
---
+++ Objective-C/HomeKitty/HomeKitty/UIColor+BNRAppColors.m
@@ -0,0 +1,17 @@
+//
+//  UIColor+BNRAppColors.m
+//  HomeKitty
+//
+//  Created by Joseph W. Dixon on 12/12/14.
+//  Copyright (c) 2014 Big Nerd Ranch. All rights reserved.
+//
+
+#import "UIColor+BNRAppColors.h"
+
+@implementation UIColor (BNRAppColors)
+
++ (UIColor *)bnr_backgroundColor {
+    return [self colorWithRed:0.2 green:0.7 blue:1 alpha:1];
+}
+
+@end
